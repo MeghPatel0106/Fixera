c3f38ab61a3f07fa459f1029bf17220e3dac3307
MeghPatel0106 <megh2850@gmail.com>
2026-04-19 05:30:36 +0530

    new

--- fixera-backend/app.py
+++ fixera-backend/app.py
@@ -1646,4 +1646,5 @@ def report_history():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
